d8d9d4b80dce2de8e393eed90d61d96d0dab50c9
ralexibm <63743225+ralexibm@users.noreply.github.com>
2020-04-15 17:12:38 -0300

    Update RHEL_post_update.sh

--- RHEL_post_update.sh
+++ RHEL_post_update.sh
@@ -3,7 +3,7 @@
 ## Variables
 
 PATH_DIR=/var/log/checklist
-BACKUP_DIR=POST-UPDATE-$(date +'%d-%m-%Y-%H-%M')
+BACKUP_DIR=POST-UPDATE-$(date +'%Y-%m-%d_%H-%M')
 
 DIR_LOGFILES=$PATH_DIR/$BACKUP_DIR
 
